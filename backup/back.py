dc1a82d6d56c53175314fc9b639f4d2829e11d2a
Rovertholley <pjmpiop@gmail.com>
2018-07-17 03:21:11 +0100

    new file:   backup/back.py 	new file:   backup/dhcpcd.conf 	new file:   backup/udhcpd.conf 	new file:   backup/wpa_supplicant.conf

---
+++ backup/back.py
@@ -0,0 +1,12 @@
+import os
+
+def _copy_file_no_overwriting(src, dst):
+    import shutil
+    if not os.path.isfile(dst):
+        print('copying... ', dst)
+        shutil.copyfile(src, dst)
+        
+dir = os.path.dirname(__file__)
+_copy_file_no_overwriting(os.path.abspath(dir + 'dhcpd.conf'), os.path.abspath('/etc/dhcp/dhcpd.conf'))
+_copy_file_no_overwriting(os.path.abspath(dir + 'udhcpd.conf'), os.path.abspath('/etc/udhcpd.conf'))
+_copy_file_no_overwriting(os.path.abspath(dir + 'wpa_supplicant.conf'), os.path.abspath('/etc/wpa_supplicant/wpa_suplicant.conf'))
